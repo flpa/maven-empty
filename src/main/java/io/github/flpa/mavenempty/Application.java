7d01e94bf158bd8643e54c4c5d97b4ec88a9cbbc
flpa <flpa.dev@zoho.com>
2019-04-28 21:10:52 +0200

    rename

--- src/main/java/io/github/flpa/mavenempty/Application.java
+++ src/main/java/io/github/flpa/mavenempty/Application.java
@@ -1,7 +1,7 @@
 package io.github.flpa.mavenempty;
 
-public class Main {
-	public Main() {
+public class Application {
+	public static void main(String[] args) {
 		System.out.println("Empty");
 	}
 }
